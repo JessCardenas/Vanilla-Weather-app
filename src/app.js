37a8cbebeaef2c23235a3d15ee17a16787bd7a3e
JessCardenas <jesscardenasbcn@gmail.com>
2024-06-26 11:39:50 +0200

    added function searchCity

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = `a1b23b3d4098a0a16ftb2b02cf94o4d8`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
