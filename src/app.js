f8b45c39523b8c49d5b0661a4e5beeaaf96ec17b
JessCardenas <jesscardenasbcn@gmail.com>
2024-06-28 12:00:03 +0200

    Injected the forecast html from js

--- src/app.js
+++ src/app.js
@@ -52,7 +52,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+          <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+           <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>29º</strong>
+              </div>
+              <div class="weather-forecast-temperature">18º</div>
+            </div>
+        </div>
+        `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Barcelona");
+displayForecast();
